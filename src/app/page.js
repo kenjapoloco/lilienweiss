05b2fc9517544e29a7c89f5eb9fc04fe808de444
Ken Tsukamoto <tsukaken0622@gmail.com>
2025-07-31 00:47:20 +0900

    ✨ヘッダー編集

--- src/app/page.js
+++ src/app/page.js
@@ -1,106 +1,42 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+   <>  
+     <header className="text-gray-700 border-b border-gray-200">
+      <div className="container flex mx-auto p-5 flex-col md:flex-row items-center">
+        <a href="#" className="font-medium text-gray-900 mb-3 md:mb-0">
+          <span className="text-xl ml-3">リリエンワイス</span>
+        </a>
+        <nav className="md:ml-auto text-base">
+          <a href="#Home" className="mr-5 hover:text-blue-400 duration-300">ホーム</a>
+          <a href="#About" className="mr-5 hover:text-blue-400 duration-300">リリエンワイスについて</a>
+          <a href="#Music" className="mr-5 hover:text-blue-400 duration-300">過去の音源</a>
+          <a href="#Schedule" className="mr-5 hover:text-blue-400 duration-300">スケジュール</a>
+          <a href="#Contact" className="hover:text-blue-400 duration-300">コンタクト</a>
+        </nav>
+      </div>
+     </header>
+     <section>
+      <div>
+        <div>
+          <h1>
+            白百合のように
+            <br />
+            清く美しく…
+          </h1>
+          <p>
+            リリエンワイス
+            <br />
+            マンドリン楽団
+          </p>
+          <p>
+            次回の定期演奏会
+            <br />
+            8月30日 愛知県芸術劇場ホール
+          </p>
+          <button>詳細はこちら</button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-      </footer>
-    </div>
-  );
+      </div>
+     </section>
+   </>  
+  )
 }
